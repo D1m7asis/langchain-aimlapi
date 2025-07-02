db72bd979519891f4fe5c806cf3ce0fd2cc3f1db
D1m7asis <tumaskow@yandex.ru>
2025-07-02 14:18:40 +0200

    Init 2

---
+++ langchain_aimlapi/__init__.py
@@ -0,0 +1,27 @@
+from importlib import metadata
+
+from langchain_aimlapi.chat_models import ChatAimlapi
+from langchain_aimlapi.document_loaders import AimlapiLoader
+from langchain_aimlapi.embeddings import AimlapiEmbeddings
+from langchain_aimlapi.retrievers import AimlapiRetriever
+from langchain_aimlapi.toolkits import AimlapiToolkit
+from langchain_aimlapi.tools import AimlapiTool
+from langchain_aimlapi.vectorstores import AimlapiVectorStore
+
+try:
+    __version__ = metadata.version(__package__)
+except metadata.PackageNotFoundError:
+    # Case where package metadata is not available.
+    __version__ = ""
+del metadata  # optional, avoids polluting the results of dir(__package__)
+
+__all__ = [
+    "ChatAimlapi",
+    "AimlapiVectorStore",
+    "AimlapiEmbeddings",
+    "AimlapiLoader",
+    "AimlapiRetriever",
+    "AimlapiToolkit",
+    "AimlapiTool",
+    "__version__",
+]
